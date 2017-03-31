87b6ce3500834e39ad0b2c00f439a414ad8288eb
Alex <abrantsma@hotmail.com>
2017-03-31 16:39:35 +0200

    hi

--- flask example.py
+++ flask example.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-	if request.method == 'POST':
-		print(request.json)
-		return '',200
-	else:
-		abort(400)
+	print('hi')
+	# if request.method == 'POST':
+	# 	print(request.json)
+	# 	return '',200
+	# else:
+	# 	abort(400)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=10010)
