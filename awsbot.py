e112cff8cc30f9193b73459018a95f03376ee19d
Alex <abrantsma@hotmail.com>
2017-03-31 15:58:36 +0200

    hi

--- awsbot.py
+++ awsbot.py
@@ -1,15 +1,10 @@
 from itty import *
 import urllib2
-from ciscosparkapi import CiscoSparkAPI
+import json
+from ciscospark import CiscoSparkAPI
 import requests
 import sys
-import json
-
-bot_email = "jukkmoddr@sparkbot.io"
-bot_name = "Test"
-bearer = "MzBjZTAzZDMtYzZmNS00YmQyLTg4MjQtMjUyOTcyNzk4NzUyNTQzNTRjODctNzFk"
-bat_signal  = "https://upload.wikimedia.org/wikipedia/en/c/c6/Bat-signal_1989_film.jpg"
-run_itty(server='wsgiref', host='0.0.0.0', port=10010)
+import os
 
 
 
@@ -25,6 +20,20 @@ def sendSparkGET(url):
     request.add_header("Authorization", "Bearer "+bearer)
     contents = urllib2.urlopen(request).read()
     return contents
+   
+def sendSparkPOST(url, data):
+    """
+    This method is used for:
+        -posting a message to the Spark room to confirm that a command was received and processed
+    """
+    request = urllib2.Request(url, json.dumps(data),
+                            headers={"Accept" : "application/json",
+                                     "Content-Type":"application/json"})
+    request.add_header("Authorization", "Bearer "+bearer)
+    contents = urllib2.urlopen(request).read()
+    return contents
+   
+
 
 @post('/')
 def index(request):
@@ -32,20 +41,27 @@ def index(request):
     When messages come in from the webhook, they are processed here.  The message text needs to be retrieved from Spark,
     using the sendSparkGet() function.  The message text is parsed.  If an expected command is found in the message,
     further actions are taken. i.e.
-    /batman - replies to the room with text
+    /batman    - replies to the room with text
     /batcave   - echoes the incoming text to the room
     /batsignal - replies to the room with an image
     """
-    spark = CiscoSparkAPI(access_token=bearer)
     webhook = json.loads(request.body)
     print webhook['data']['id']
     result = sendSparkGET('https://api.ciscospark.com/v1/messages/{0}'.format(webhook['data']['id']))
-    room_id = str(webhook["data"]["roomId"])
     result = json.loads(result)
+    msg = None
     if webhook['data']['personEmail'] != bot_email:
         in_message = result.get('text', '').lower()
         in_message = in_message.replace(bot_name, '')
         if 'start' in in_message:
-            spark.messages.create(roomId=room_id, text= "Test will initiate")
+            msg = "Test will initiate"
+            sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": webhook['data']['roomId'], "text": msg})
+    return "true"
 
 
+####CHANGE THESE VALUES#####
+bot_email = "jukkmoddr@sparkbot.io"
+bot_name = "Test"
+bearer = "ZTlkYzU2MGQtNWZlOC00MTA5LWE5MzYtMjdiYjMyNzA5NTYzNjBlYjU3NWMtYjg4"
+bat_signal  = "https://upload.wikimedia.org/wikipedia/en/c/c6/Bat-signal_1989_film.jpg"
+run_itty(server='wsgiref', host='0.0.0.0', port=10010)
