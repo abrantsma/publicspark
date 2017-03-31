d5f64f3ddbac7ab7ee82b7be4680b7a79fe7ab19
Alex <abrantsma@hotmail.com>
2017-03-31 15:42:09 +0200

    hi

--- awsbot.py
+++ awsbot.py
@@ -4,7 +4,26 @@ import requests
 import sys
 import json
 
+bot_email = "jukkmoddr@sparkbot.io"
+bot_name = "Test"
+bearer = "ZTlkYzU2MGQtNWZlOC00MTA5LWE5MzYtMjdiYjMyNzA5NTYzNjBlYjU3NWMtYjg4"
+bat_signal  = "https://upload.wikimedia.org/wikipedia/en/c/c6/Bat-signal_1989_film.jpg"
+run_itty(server='wsgiref', host='0.0.0.0', port=10010)
 
+spark = CiscoSparkAPI(access_token=bearer)
+
+def sendSparkGET(url):
+    """
+    This method is used for:
+        -retrieving message text, when the webhook is triggered with a message
+        -Getting the username of the person who posted the message if a command is recognized
+    """
+    request = urllib2.Request(url,
+                            headers={"Accept" : "application/json",
+                                     "Content-Type":"application/json"})
+    request.add_header("Authorization", "Bearer "+bearer)
+    contents = urllib2.urlopen(request).read()
+    return contents
 
 @post('/')
 def index(request):
@@ -17,12 +36,14 @@ def index(request):
     /batsignal - replies to the room with an image
     """
     webhook = json.loads(request.body)
-    print webhook
+    print webhook['data']['id']
+    result = sendSparkGET('https://api.ciscospark.com/v1/messages/{0}'.format(webhook['data']['id']))
     room_id = webhook["data"]["roomId"]
+    result = json.loads(result)
+    if webhook['data']['personEmail'] != bot_email:
+        in_message = result.get('text', '').lower()
+        in_message = in_message.replace(bot_name, '')
+        if 'start' in in_message:
+            spark.messages.create(roomId=room_id, text= "Test will initiate")
 
-####CHANGE THESE VALUES#####
-bot_email = "jukkmoddr@sparkbot.io"
-bot_name = "Test"
-bearer = "ZTlkYzU2MGQtNWZlOC00MTA5LWE5MzYtMjdiYjMyNzA5NTYzNjBlYjU3NWMtYjg4"
-bat_signal  = "https://upload.wikimedia.org/wikipedia/en/c/c6/Bat-signal_1989_film.jpg"
-run_itty(server='wsgiref', host='0.0.0.0', port=10010)
+
