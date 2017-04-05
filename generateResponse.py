3761efb53f82e50d662fbf6adf685201298906ab
Alex <abrantsma@hotmail.com>
2017-04-05 16:25:48 +0200

    hi

--- generateResponse.py
+++ generateResponse.py
@@ -1,3 +1,3 @@
 # test 
 def generateResponse(message, personEmail, bot_email)
-	spark.messages.create(toPersonEmail=message.personEmail, text="processing...")
+	spark.messages.create(toPersonEmail=message, text="processing...")
