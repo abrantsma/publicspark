0073329685f81dcfd1bb73558f5d4340dc7d2fd2
christimestwo <chris.tsiourakis@gmail.com>
2017-04-06 13:14:10 +0200

    Add files via upload

--- DBconnect.py
+++ DBconnect.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 
 conn = mysql.connector.connect(user='brainspark', password='C!sco123',
-                              host='brainspark.cptvcix7ijfy.us-west-2.rds.amazonaws.com:3306',
+                              host='brainspark.cptvcix7ijfy.us-west-2.rds.amazonaws.com',
                               database='brainspark')
 mycursor=conn.cursor()
 mycursor.execute("SHOW TABLES")
