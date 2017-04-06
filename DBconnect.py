72b371b1bad0b9c1ae49472dac17167a0746ea1d
christimestwo <chris.tsiourakis@gmail.com>
2017-04-06 15:33:22 +0200

    Add files via upload

--- DBconnect.py
+++ DBconnect.py
@@ -6,5 +6,7 @@ conn = mysql.connector.connect(user='brainspark', password='C!sco123',
                               database='brainspark')
 mycursor=conn.cursor()
 mycursor.execute("INSERT INTO ideas VALUES (1,'my idea is that Alex sucks')")
+mycursor.execute("INSERT INTO ideas VALUES (2,'my idea is that Ali sucks')")
+mycursor.execute("INSERT INTO ideas VALUES (3,'my idea is that Chris is nice')")
 conn.commit()
 print(mycursor.fetchall())
