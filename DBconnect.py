03bd70a14aaf3613477482ef99886333f23f6ecc
christimestwo <chris.tsiourakis@gmail.com>
2017-04-06 16:35:23 +0200

    Add files via upload

--- DBconnect.py
+++ DBconnect.py
@@ -1,12 +1,15 @@
 import mysql.connector
 
-
-conn = mysql.connector.connect(user='brainspark', password='C!sco123',
+def sendToDatabase(message, personEmail, bot):
+	conn = mysql.connector.connect(user='brainspark', password='C!sco123',
                               host='brainspark.cptvcix7ijfy.us-west-2.rds.amazonaws.com',
                               database='brainspark')
-mycursor=conn.cursor()
-mycursor.execute("INSERT INTO ideas VALUES (1,'my idea is that Alex sucks')")
-mycursor.execute("INSERT INTO ideas VALUES (2,'my idea is that Ali sucks')")
-mycursor.execute("INSERT INTO ideas VALUES (3,'my idea is that Chris is nice')")
-conn.commit()
-print(mycursor.fetchall())
+	mycursor=conn.cursor()
+	mycursor.execute("INSERT INTO ideas (userID,q1) VALUES (?,?)", (personEmail,message))
+	conn.commit()
+# print(mycursor.fetchall())
+
+def pullFromDatabase(message, personEmail):
+
+
+sendToDatabase("hallo", "ctsioura@cisco.com", "test@cisco.com")
