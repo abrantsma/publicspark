8bb175ffa00d6578e27c7b45c29dbc9c6ec10cc1
christimestwo <chris.tsiourakis@gmail.com>
2017-04-07 10:22:41 +0200

    Add files via upload

--- DBconnect.py
+++ DBconnect.py
@@ -11,15 +11,18 @@ def sendToDatabase(message, personEmail, bot):
 # print(mycursor.fetchall())
 
 def pullFromDatabase(message, personEmail):
+	conn = mysql.connector.connect(user='brainspark', password='C!sco123',
+                              host='brainspark.cptvcix7ijfy.us-west-2.rds.amazonaws.com',
+                              database='brainspark')
 	mycursor=conn.cursor()
-	mycursor.execute("SELECT * FROM ideas")
-	a = mycursor.execute(sql)
-	print("The bot's adress is",a)
+	a = mycursor.execute("SELECT * FROM ideas")
+	# a = mycursor.execute(sql)
+	# print("The bot's adress is",a)
 	data = a.fetchall()
 	print(data)
 	conn.commit()
 	return "true"
 
 
-sendToDatabase("hallo", 65745, "test@cisco.com")
+# sendToDatabase("hallo", 65745, "test@cisco.com")
 pullFromDatabase("message", "personEmail")
