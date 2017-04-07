6c531b17d9926e8094fe142f9e35f706b50f7420
christimestwo <chris.tsiourakis@gmail.com>
2017-04-07 11:15:07 +0200

    Add files via upload

--- DBconnect.py
+++ DBconnect.py
@@ -15,7 +15,7 @@ def pullFromDatabase(message, personEmail):
                               host='brainspark.cptvcix7ijfy.us-west-2.rds.amazonaws.com',
                               database='brainspark')
 	mycursor=conn.cursor()
-	sql = "SELECT * FROM ideas WHERE q2 '%s'" % ("test@cisco.com")
+	sql = "SELECT * FROM ideas WHERE q2 '%s'" % ('test@cisco.com')
 	try:
 		mycursor.execute(sql)
 		results = mycursor.fetchall()
