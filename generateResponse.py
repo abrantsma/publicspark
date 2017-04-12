986e2f49a4ac07671bb04c39156ee2e67118777f
Alex <abrantsma@hotmail.com>
2017-04-12 14:46:49 +0200

    added questions

--- generateResponse.py
+++ generateResponse.py
@@ -1,3 +1,11 @@
 # test 
 def generateResponse(message, personEmail, bot_email):
+	pullFromDatabase(personEmail)
+	Q1 = "question1"
+	Q2 = "question2"
+	Q3 = "question3"
+	Q4 = "question4"
+	Q5 = "question5"
+	Q6 = "question6"
+	Q7 = "question7"
 	return("response")
